a7a0af10e51dfd65961a1b6153aa305fede0d9dd
João Vieira <joaobernado888@gmail.com>
2022-12-29 13:29:00 +0000

    Trying put carousel component diretly on App.js more folders and files like for exemple: pag, main.js, etc

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import './App.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
 
+//this to change
 function App() {
     return (
         <div className="App">
-            <h1>JBSOFTSOL</h1>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">JBSOFTSOL</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Main</Nav.Link>
+                        <Nav.Link href="#link">contacts</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Actions</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+            <Carousel variant="dark">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=First slide&bg=f5f5f5"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Second slide&bg=eee"
+                    alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+                    alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                    <h5>Third slide label</h5>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
+
 
 export default App;
