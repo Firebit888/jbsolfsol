68f08170e6a589e998d3e35090e7c5793e0b650c
João Vieira <joaobernado888@gmail.com>
2023-02-23 14:18:53 +0000

    Add more decoration

--- src/views/App.js
+++ src/views/App.js
@@ -7,13 +7,27 @@ import Comecar from './Pag/main';
 import Carrou from './Pag/carrossel_pag/slideMain';
 //acordation
 import BasicExample from './Pag/Acordations/Accordat';
+//buttons
+import OutlineTypesExample from './Pag/buttons/showm';
 
 function App() {
     return (
         <>
             <Comecar />
             <Carrou />
+            <div>
+                <h2>Welcome to the main page</h2>
+                <p>
+                   <h4>You can write everything here</h4> 
+                </p>
+            </div>
             <BasicExample />
+            <div className='af'>
+                <h3>JBSOFTSOL FOR ALL SOLUTIONS <OutlineTypesExample /></h3>
+            </div>
+            <div className='ag'>
+                <a>Categories: </a ><a className='pts' href='#'> Main </a> <a className='pts' href='#'>| Contacts </a> <a className='pts' href='#'>| Others </a>
+            </div>
         </>
     );
 }
---
+++ src/views/Pag/buttons/showm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function OutlineTypesExample() {
+  return (
+    <>
+      <Button variant="outline-primary">Show more</Button>{' '}
+    </>
+  );
+}
+
+export default OutlineTypesExample;
